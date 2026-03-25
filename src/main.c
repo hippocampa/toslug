00a924ab03824c2664b453a067684a3411ead142
hippocampa <tsdhrm@outlook.com>
2026-03-25 11:12:08 +0800

    fix(cli): adding standard error and improve helpText with static const

--- src/main.c
+++ src/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-const char *helpText = 
+static const char helpText[] = 
   "toslug\n\n"
   "usage:\n"
   "toslug <text> <flags>\n\n"
   "flags:\n"
   "-h\t--use-hyphen\tUse hyphen separator. (default)\n"
   "-u\t--use-underscore\tUse underscore separator.";
-;
+
 
 
 int main(int argc, char *argv[]){
   if (argc < 2) {
-    printf("%s\n", helpText);
-
+    fprintf(stderr, "%s\n", helpText);
+    return EXIT_FAILURE;
   }
+  return EXIT_SUCCESS;
 }
